cbaf4051b64122a81e7d9e811a86383dc51eeec8
theophilhenry <s160419078@student.ubaya.ac.id>
2021-08-18 17:14:57 +0800

    W2 Selesai

--- W2-RoutingAndView/resources/views/welcome.blade.php
+++ W2-RoutingAndView/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Toko Roti Theophil HS
                 </div>
 
                 <div class="links">
